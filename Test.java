d177bb10e96819b282758aace64f07c878a00c21
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-13 19:37:40 +0530

    hello

--- Test.java
+++ Test.java
@@ -94,7 +94,7 @@ public class Test {
         }
     }
 
-    public static void findUnionNIntersection(int[] arr1, int[] arr2) { //Q6
+    public static void findUnionNIntersection(int[] arr1, int[] arr2) { // Q6
         System.out.println("Intersection elements ");
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
@@ -131,27 +131,38 @@ public class Test {
 
     }
 
-    public static  void cyclicallyRotate(int[] arr){ // Q7
+    public static void cyclicallyRotate(int[] arr) { // Q7
         int n = arr.length;
-        int last = arr[n-1];
+        int last = arr[n - 1];
 
-        for(int i =n-1;i>0;i--){
-            arr[i]= arr[i-1];
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
         }
         arr[0] = last;
-        for(int i =0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
 
+    public static void KandansAlogrithm(int[] arr) { // Q8
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currSum = currSum + arr[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(maxSum, currSum);
+        }
+        System.out.println(maxSum);
+       
+    }
 
-
+    
 
     public static void main(String[] args) {
-        int[] arr1 = { 1, 2, 3, 4, 5, 7 };
-       
-
-    cyclicallyRotate(arr1);
+        int[] arr = { 1, 2, 3, 4, 5, 7 };
+       KandansAlogrithm(arr);
     }
 
 }
