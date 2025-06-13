2e60c945234288770814828f7fed05a11425c906
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-13 19:26:55 +0530

    hello

--- Test.java
+++ Test.java
@@ -94,7 +94,7 @@ public class Test {
         }
     }
 
-    public static void findUnionNIntersection(int[] arr1, int[] arr2) {
+    public static void findUnionNIntersection(int[] arr1, int[] arr2) { //Q6
         System.out.println("Intersection elements ");
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
@@ -131,11 +131,27 @@ public class Test {
 
     }
 
+    public static  void cyclicallyRotate(int[] arr){ // Q7
+        int n = arr.length;
+        int last = arr[n-1];
+
+        for(int i =n-1;i>0;i--){
+            arr[i]= arr[i-1];
+        }
+        arr[0] = last;
+        for(int i =0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+
+
+
     public static void main(String[] args) {
         int[] arr1 = { 1, 2, 3, 4, 5, 7 };
-        int[] arr2 = { 1, 2, 3, 4,5,8 };
+       
 
-        findUnionNIntersection(arr1, arr2);
+    cyclicallyRotate(arr1);
     }
 
 }
