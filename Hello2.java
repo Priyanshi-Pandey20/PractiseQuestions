7b841f4a8ccbe74d8754ccacb2cf2d266f635d42
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-29 10:50:21 +0530

    hello

--- Hello2.java
+++ Hello2.java
@@ -419,6 +419,48 @@ public class Hello2 {
         }
     }
 
+
+      public static void spiralPrint(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int top = 0;
+        int bottom = rows - 1;
+        int left = 0;
+        int right = cols - 1;
+
+        while (top <= bottom && left <= right) {
+            // Print top row
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            // Print right column
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            // Print bottom row
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            // Print left column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+
+
     
 
 
