fe319ee15cfb7e1831b02a5cf72e8e47e8c80417
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-10-28 20:52:43 +0530

    hi

--- Hello3.java
+++ Hello3.java
@@ -1,39 +1,40 @@
 import java.util.*;
+
 public class Hello3 {
 
-    public static int[] twoSum(int[]nums,int target){
-        for(int i = 0;i<nums.length-1;i++){
-            for(int j = 0;j<nums.length;j++){
-                if(nums[i]+nums[j] == target){
-                    return new int[] {i,j};
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }
         return new int[] {};
-    } 
+    }
 
-    public static boolean containsDuplicates(int[] nums){  // sorting approach
+    public static boolean containsDuplicates(int[] nums) { // sorting approach
         Arrays.sort(nums);
         System.out.println(Arrays.toString(nums));
-        for(int i = 0;i<nums.length-1;i++){
-            for (int j = i+1;j< nums.length;j++){
-                if(nums[i] == nums[j]){
-                   return true;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
                 }
             }
         }
-      return false;
+        return false;
     }
 
-    public static boolean containsDuplicates1(int[] nums){ // hashet approach 
+    public static boolean containsDuplicates1(int[] nums) { // hashet approach
 
-        HashSet<Integer>set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
-         for (int i = 0; i < nums.length; i++)
-          nums[i] = i;
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = i;
 
-        for(int num:nums){
-            if(set.contains(num)){
+        for (int num : nums) {
+            if (set.contains(num)) {
                 return false;
             }
             set.add(num);
@@ -41,131 +42,159 @@ public class Hello3 {
         return true;
     }
 
-
-    public static void moveZeroes(int[] nums){  // optimal
+    public static void moveZeroes(int[] nums) { // optimal
         int nonzero = 0;
 
-        for (int i = 0;i<nums.length;i++){
-            if(nums[i] != 0){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
                 int temp = nums[nonzero];
                 nums[nonzero] = nums[i];
                 nums[i] = temp;
-                 nonzero++;
+                nonzero++;
             }
-          
+
         }
     }
 
-    public static void reverseString(char[] s){
+    public static void reverseString(char[] s) {
         int start = 0;
-        int end = s.length-1;
-          
-     while(start<end){
-        char temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+        int end = s.length - 1;
 
-        start++;
-        end--;
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
 
-     }
+            start++;
+            end--;
+
+        }
     }
 
-    public static String reverseWords(String str){
+    public static String reverseWords(String str) {
         char[] s = str.toCharArray();
         int start = 0;
 
-        for(int i = 0;i<=s.length;i++){
-            if(i == s.length || s[i] == ' '){
-                reversePart(s,start,i-1);
-                start = i+1;
+        for (int i = 0; i <= s.length; i++) {
+            if (i == s.length || s[i] == ' ') {
+                reversePart(s, start, i - 1);
+                start = i + 1;
 
             }
         }
         return new String(s);
     }
 
-    public static void reversePart(char[] s,int start,int end){
-        while(start<end){
-             char temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+    public static void reversePart(char[] s, int start, int end) {
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
 
-        start++;
-        end--;
+            start++;
+            end--;
         }
     }
 
-    public static void mergeTwoSortedArray(int[] num1,int m,int[] num2,int n){  //brute force 
-         m = num1.length;
-         n = num2.length;
+    public static void mergeTwoSortedArray(int[] num1, int m, int[] num2, int n) { // brute force
+        m = num1.length;
+        n = num2.length;
 
-         for(int j = 0, i = m;j<n;j++){
+        for (int j = 0, i = m; j < n; j++) {
             num1[i] = num2[j];
             i++;
-         }
-         Arrays.sort(num1);
+        }
+        Arrays.sort(num1);
 
     }
 
-    public static int[] mergeTwoSortedArray1(int[] num1,int[] num2){
-      int m = 3;
-       int n = 3;
-        int i = m-1;
-        int k = m+n-1;
-        int j = n-1;
-        while(j>=0){
-            if(i>=0 && num1[i] > num2[j]){
+    public static int[] mergeTwoSortedArray1(int[] num1, int[] num2) {
+        int m = 3;
+        int n = 3;
+        int i = m - 1;
+        int k = m + n - 1;
+        int j = n - 1;
+        while (j >= 0) {
+            if (i >= 0 && num1[i] > num2[j]) {
                 num1[k] = num1[i];
                 k--;
                 i--;
-            }
-            else{
+            } else {
                 num1[k] = num2[j];
-                k--;j--;
+                k--;
+                j--;
             }
         }
         return num1;
 
-    
     }
 
+    public static boolean isPalindrome(String s){
+        String str = s.replaceAll("[A-Za-z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(str).reverse().toString();
 
-
-
-    public static void main(String[]args){
-    int[] nums = {0,1,0,3,12};
-    // int target = 5;
-    // int[] result= twoSum(nums, target);  
-    // System.out.println(Arrays.toString(result));
-   
-    //  boolean hasduplicates = containsDuplicates1(nums);
-    //  System.out.println(hasduplicates);
-    moveZeroes(nums);
-
-    for(int num :nums){
-        System.out.println(num);
+        if(str.equals(reversed)){
+            return true;
+        }else if(str == ""){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    char[] s = {'h','e','l','l','o'};
-    reverseString(s);
-    for (char c : s) {
-    System.out.print(c);  // olleh
-}
+     public static boolean validPalindrom(String str){
+        int left = 0;int right = str.length()-1;
+        while(left<right){
+            if(str.charAt(left) != str.charAt(right)){
+                return checkPalindrom(str, left + 1, right) || checkPalindrom(str, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+     }
 
-String str = "Let's take Leetcode contest";
- String res = reverseWords(str);
- System.out.println(res);
+     public static boolean checkPalindrom(String str,int left,int right){
+        while(left<right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+     }
 
- int[] num1 = {1,2,3,0,0,0};
- int[] num2 = {2,5,6};
 
- int[] result = mergeTwoSortedArray1(num1, num2);
+    public static void main(String[] args) {
+        // int[] nums = { 0, 1, 0, 3, 12 };
+        // // int target = 5;
+        // // int[] result= twoSum(nums, target);
+        // // System.out.println(Arrays.toString(result));
 
- System.out.println(Arrays.toString(result));  
+        // // boolean hasduplicates = containsDuplicates1(nums);
+        // // System.out.println(hasduplicates);
+        // moveZeroes(nums);
 
+        // for (int num : nums) {
+        //     System.out.println(num);
+        // }
 
-     
+        // char[] s = { 'h', 'e', 'l', 'l', 'o' };
+        // reverseString(s);
+        // for (char c : s) {
+        //     System.out.print(c); // olleh
+        // }
+
+        String str = "abc";
+      boolean result = validPalindrom(str);
+      System.out.println(result);
+        // int[] num1 = { 1, 2, 3, 0, 0, 0 };
+        // int[] num2 = { 2, 5, 6 };
+
+        // int[] result = mergeTwoSortedArray1(num1, num2);
+
+        // System.out.println(Arrays.toString(result));
 
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,3 +1,6 @@
-int target = 5;
-    // int[] result= twoSum(nums, target);  
-    // System.out.println(Arrays.toString(result));
+  // int[] num1 = { 1, 2, 3, 0, 0, 0 };
+        // int[] num2 = { 2, 5, 6 };
+
+        // int[] result = mergeTwoSortedArray1(num1, num2);
+
+        // System.out.println(Arrays.toString(result));
